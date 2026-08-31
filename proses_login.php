7349e8466a22ab739cc04e3bd4ea21269c3fe0a0
Bayu Abu Yazid <bayusj98@gmail.com>
2026-08-31 09:34:28 +0700

    mengubah file proses

--- proses_login.php
+++ proses_login.php
@@ -1,10 +1,19 @@
 <?php
 
+$session_start();
+include "koneksi.php";
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username == 'bayu' && $password == '1234') {
-    echo "Login berhasil!";
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($koneksi, $query);
+
+if (mysqli_num_rows($result) > 0) {
+
+    $user = mysqli_fetch_assoc($result);
+    $_SESSION['user'] = $user;
+    header("Location: dashboard.php");
 } else {
     echo "Login gagal. Silakan coba lagi.";
 }
