ec26d5baf7741ec2e731d45e32bc8ebdad3054bb
Bayu Abu Yazid <bayusj98@gmail.com>
2026-09-01 14:49:30 +0700

    memperbaiki proses login

--- proses_login.php
+++ proses_login.php
@@ -6,15 +6,26 @@ include "koneksi.php";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$query = "SELECT * FROM users WHERE username='$username'";
 $result = mysqli_query($koneksi, $query);
 
 if (mysqli_num_rows($result) > 0) {
 
     $user = mysqli_fetch_assoc($result);
-    $_SESSION['user'] = $user;
-    header("Location: dashboard.php");
+
+    if (password_verify($password, $user['password'])) {
+
+        $_SESSION['user'] = $user;
+
+        header("Location: dashboard.php");
+        exit();
+
+    } else {
+        echo "Password salah.";
+    }
+
 } else {
-    echo "Login gagal. Silakan coba lagi.";
+    echo "Username tidak ditemukan.";
 }
+
 ?>
